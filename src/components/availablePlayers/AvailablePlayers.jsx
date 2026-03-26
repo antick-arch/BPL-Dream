3fdfc563667076e718115d6674de6e308f81d1f2
antick-arch <e241003@grad.iiuc.ac.bd>
2026-03-26 18:09:29 +0600

    added toggoling sunctionalities

--- src/components/availablePlayers/AvailablePlayers.jsx
+++ src/components/availablePlayers/AvailablePlayers.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import { use } from 'react';
+import { use, useState } from 'react';
 import Card from '../card/Card';
+import SelectedPlayers from '../selectedPlayers/selectedPlayers';
 const AvailablePlayers = ({ fetchPlayerPromise }) => {
-        const playerData = use(fetchPlayerPromise).data;
+    const playerData = use(fetchPlayerPromise).data;
+    const [selectedType, setSelectedType] = useState('available');
     return (
-        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-            {
-                playerData.map((players,index)=> <Card key={index} players={players}></Card>)
-            }
+        <div>
+            <div className='flex items-center justify-between my-6'>
+                {selectedType === 'available'?(<h2 className='font-bold text-3xl'>Available Players</h2>):(<h2 className='font-bold text-3xl'>Selected Players</h2>)}
+                <div className='flex items-center'>
+                    <button onClick={() => setSelectedType('available')} className={`btn ${selectedType === 'available' ? 'bg-[#E7FE29]' : ''} border-none rounded-l-xl rounded-r-none`}>Available</button>
+                    <button onClick={() => setSelectedType('selected')} className={`btn ${selectedType === 'selected' ? 'bg-[#E7FE29]' : ''} border-none rounded-r-xl rounded-l-none`}>Selected <span>(0)</span></button>
+                </div>
+            </div>
+
+
+            {selectedType === 'available'?(<Card playerData={playerData}></Card>):(<SelectedPlayers></SelectedPlayers>)}
         </div>
     );
 };
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,33 +1,45 @@
 import { CircleUserRound, Flag } from 'lucide-react';
 import React from 'react';
-const Card = ({ players }) => {
-    return (
-        <div className="card bg-base-100 shadow-sm">
-            <figure className="px-6 pt-6">
-                <img
-                    src={players.playerImg}
-                    alt="Shoes"
-                    className="rounded-xl border border-blue-300" />
-            </figure>
-            <div className="card-body space-y-2">
-                <h2 className="card-title font-bold"><CircleUserRound />{players.playerName}</h2>
-                <div className='flex justify-between items-center'>
-                    <p className='flex gap-1 text-[0.9rem] text-gray-400 font-medium'><Flag />{players.playerCountry}</p>
-                    <button className='btn'>{players.playerType}</button>
-                </div>
-                <div className="divider"></div>
-                <h2 className='text-[1rem] font-bold'>Rating</h2>
-                <div className='flex justify-between'>
-                    <h2 className='text-[1rem] font-bold'>{players.battingStyle}</h2>
-                    <h2 className='text-[1rem] text-gray-400 font-medium'>{players.bowlingStyle}</h2>
-                </div>
-                <div className="flex items-center justify-between">
-                    <h2 className='text-[1rem] font-bold'>Price: ${players.price}</h2>
-                    <button className="btn">Buy Now</button>
-                </div>
+
+const Card = ({ playerData }) => {
+  return (
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+      {playerData.map((player,index) => (
+        <div key={index} className="card bg-base-100 shadow-sm">
+          <figure className="px-6 pt-6">
+            <img
+              src={player.playerImg}
+              alt={player.playerName}
+              className="rounded-xl border border-blue-300"
+            />
+          </figure>
+          <div className="card-body space-y-2">
+            <h2 className="card-title font-bold flex items-center gap-2">
+              <CircleUserRound /> {player.playerName}
+            </h2>
+            <div className="flex justify-between items-center">
+              <p className="flex gap-1 text-[0.9rem] text-gray-400 font-medium">
+                <Flag /> {player.playerCountry}
+              </p>
+              <button className="btn">{player.playerType}</button>
             </div>
+            <div className="divider"></div>
+            <h2 className="text-[1rem] font-bold">Rating</h2>
+            <div className="flex justify-between">
+              <h2 className="text-[1rem] font-bold">{player.battingStyle}</h2>
+              <h2 className="text-[1rem] text-gray-400 font-medium">
+                {player.bowlingStyle}
+              </h2>
+            </div>
+            <div className="flex items-center justify-between">
+              <h2 className="text-[1rem] font-bold">Price: ${player.price}</h2>
+              <button className="btn bg-white">Choose Player</button>
+            </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Card;
